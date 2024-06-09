cbb38065468f8f0f25e3f32423beb3d71e7f72fa
Gertjan Idema <devel@gertjanidema.nl>
2024-06-09 12:38:43 +0200

    Added NeTeX transport area DTO

---
+++ src/main/java/nl/haltedata/netex/dto/NetexTransportArea.java
@@ -0,0 +1,18 @@
+package nl.haltedata.netex.dto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(schema="netex")
+@Getter
+@Setter
+public class NetexTransportArea {
+    private String agencyId;
+    @Id
+    private String transportArea;
+    private String concession;
+}
---
+++ src/main/java/nl/haltedata/netex/dto/NetexTransportAreaRepository.java
@@ -0,0 +1,10 @@
+package nl.haltedata.netex.dto;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface NetexTransportAreaRepository extends CrudRepository<NetexTransportArea, String> {
+
+    //
+    
+}
+
