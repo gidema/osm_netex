d796a67bd7d632f83023669e5c79f8e06b3ddbbd
gertjan <gertjan@gertjan-DESKTOP>
2024-06-18 10:05:04 +0200

    Moved MainConfiguration.class to main package nl.haltedata

---
+++ src/main/java/nl/haltedata/MainConfiguration.java
@@ -0,0 +1,30 @@
+package nl.haltedata;
+
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.PrecisionModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import nl.haltedata.tools.RdToWgs84Transformation;
+
+@Configuration
+public class MainConfiguration {
+    @SuppressWarnings("static-method")
+    @Bean
+    GeometryFactory wsgGeometryFactory() {
+        return new GeometryFactory(new PrecisionModel(), 4326);
+    }
+    
+    @SuppressWarnings("static-method")
+    @Bean
+    GeometryFactory rdGeometryFactory() {
+        return new GeometryFactory(new PrecisionModel(), 28992);
+    }
+    
+    @SuppressWarnings("static-method")
+    @Bean
+    RdToWgs84Transformation transformation() {
+        return new RdToWgs84Transformation();
+    }
+
+}
