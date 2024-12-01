a94c8a7865eca3d5a0fc59409f361eaf0057b843
gertjan <gertjan@gertjan-DESKTOP>
2024-12-01 09:52:23 +0100

    Removed RouteMatch

--- src/main/java/nl/haltedata/compare/dto/RouteMatchRepository.java
+++
@@ -1,9 +0,0 @@
-package nl.haltedata.compare.dto;
-
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface RouteMatchRepository extends CrudRepository<RouteMatch, RouteMatchId> {
-    List<RouteMatch> findByOsmRouteId(Long osmRouteId);
-}
